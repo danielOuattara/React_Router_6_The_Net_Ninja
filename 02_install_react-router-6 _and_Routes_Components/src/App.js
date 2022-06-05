29e553c6faa875cf9f6441fa108880fb74a396ac
daniel ouattara <daniel.ouattara@gmx.com>
2022-06-05 16:26:13 +0200

    routes components and react-router hook

---
+++ 02_install_react-router-6 _and_Routes_Components/src/App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { Home, About, Products, ProductDetails } from "./pages";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <nav>
+          <h1>The Ninja Clothing Company</h1>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/products">Products</Link>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/products/:id" element={<ProductDetails />} />
+          <Route path="/products" element={<Products />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ 02_install_react-router-6 _and_Routes_Components/src/pages/index.js
@@ -0,0 +1,7 @@
+import About from "./About";
+import Home from "./Home";
+import Products from "./Products";
+import ProductDetails from "./ProductDetails";
+import Offers from "./Offers";
+
+export { About, Home, Products, ProductDetails, Offers };
