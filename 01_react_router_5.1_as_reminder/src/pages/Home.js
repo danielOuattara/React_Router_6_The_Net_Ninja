405bdd5fe01df4710556a1aef1aa428b32acb28c
danielOuattara <daniel.ouattara@gmx.com>
2023-04-10 22:22:25 +0200

    rename & logic updated

--- 01_react_router_5.1_as_reminder/src/pages/Home.js
+++ 01_react_router_5.1_as_reminder/src/pages/Home.js
@@ -5,9 +5,10 @@ export default function Home() {
       <p>Quality Clothing for Quality Ninjas</p>
       <img src="https://via.placeholder.com/1200x200" alt="site banner" />
       <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima animi nulla eveniet. At non
-        consequuntur perferendis id voluptatem sapiente dolor animi exercitationem, commodi officiis
-        eveniet laudantium quidem dolore labore sit.
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima animi
+        nulla eveniet. At non consequuntur perferendis id voluptatem sapiente
+        dolor animi exercitationem, commodi officiis eveniet laudantium quidem
+        dolore labore sit.
       </p>
     </div>
   );
